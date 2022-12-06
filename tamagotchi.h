cf403b5c7455ffda00c027127c273cf0399567a0
Jimin Kim <0305jimin@gmail.com>
2022-12-06 19:05:05 +0900

    Feat: Tamagotchi 클래스 멤버변수 private 키워드 추가 #4

--- tamagotchi.h
+++ tamagotchi.h
@@ -1,11 +1,12 @@
 
-#ifndef __TAMAGOTCHI_H__ 
-#define __TAMAGOTCHI_H__ 
+#ifndef __TAMAGOTCHI_H__
+#define __TAMAGOTCHI_H__
 
 #include <iostream>
 using namespace std;
 class Tamagotchi
 {
+private:
     string name;
     string introduction;
     int xp;
